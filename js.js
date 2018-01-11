4d71fadc71e63391861fa148a4f932094fc24a10
Irwin <irwin.litvak@gmail.com>
2018-01-10 20:10:36 -0500

    Weather App now retrieves data successfully

--- js.js
+++ js.js
@@ -13,13 +13,17 @@ function searchWeather() {
     // setting up an http request
     var http = new XMLHttpRequest();
     var apiKey = "8afd931381e00062465cd0cc299c2270";
-    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName;
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + cityName + "&units=metric&appid=" + apiKey;
     var method = "GET";
     http.open(method, url);
     
     http.onreadystatechange = function(){
-        if(http.readyState === http.XMLHttpRequest.DONE && http.status === 200){
+        if(http.readyState === XMLHttpRequest.DONE && http.status === 200){
             var data = JSON.parse(http.responseText);
+            var weatherData = new Weather(cityName, data.weather[0].description.toUpperCase());
+            weatherData.temperature = data.main.temp;
+            
+            console.log(weatherData);
         } 
         else if(http.readyState === XMLHttpRequest.DONE ){
             alert("Something went wrong!");
